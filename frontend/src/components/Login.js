f833b271b2bebc885abb3929b2d989c8da71f3e5
Pranav0210 <pranavpatel.pranshu@gmail.com>
2022-11-27 01:03:14 +0530

    server changes

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -2,8 +2,36 @@ import React from "react";
 import { Link } from "react-router-dom";
 import logo1 from "./images/logo1.png";
 import "./Auth.css";
+import { useState } from "react";
+const {hashSync} = require('bcryptjs')
 
 const Login = () => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const HandleLogin = async (e) => {
+    e.preventDefault();
+    var hashedPass = hashSync(password,10)
+    console.log(hashedPass)
+    fetch('http://localhost:5000/login',{
+      method:'POST',
+      headers: {
+        'Content-type': 'application/json',
+      },
+      body:JSON.stringify({
+        'email' : email,
+        'password' : hashedPass
+      })
+    })
+    .then((response)=>{
+      console.log(response.body)
+      // document.getElementById('regForm').reset()
+    })
+  }
+
+  const HandleSignup = () => {
+
+  }
   return (
     <>
       <Link to="/">
@@ -19,23 +47,30 @@ const Login = () => {
         <div className="login">
           <div className="header">Login To Your Account</div>
           <div className="fields">
-            <input type="text" placeholder="Email" />
-            <input type="password" placeholder="Password" />
-            <Link to="/">
-              <button type="button" className="login-btn">
-                Login
-              </button>
-            </Link>
+            <input type="text" id='email'
+              name='email'
+              value={email}
+              required={true}
+              onChange={(e) => setEmail(e.target.value)}
+              placeholder="Email" />
+            <input type="password"
+              id='password'
+              name='password'
+              value={password}
+              required={true}
+              onChange={(e) => setPassword(e.target.value)}
+              placeholder="Password" />
+            <button type="button" className="login-btn" onClick={HandleLogin}>
+              Login
+            </button>
           </div>
         </div>
         <div className="signup">
           <div className="header">New Here?</div>
           <div className="para">Create a new account and enjoy shopping!</div>
-          <Link to="/signup">
-            <button type="button" className="signup-btn">
-              Signup
-            </button>
-          </Link>
+          <button type="button" className="signup-btn" onClick={HandleSignup}>
+            Signup
+          </button>
         </div>
       </div>
     </>
--- frontend/src/components/Signup.js
+++ frontend/src/components/Signup.js
@@ -1,9 +1,33 @@
-import React from "react";
+import React, {useState} from "react";
 import { Link } from "react-router-dom";
 import logo1 from "./images/logo1.png";
 import "./Auth.css";
+import { hashSync } from "bcryptjs";
 
 const Signup = () => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [confpassword, setConfPassword] = useState('')
+
+  const HandleSignup = async (e) => {
+    e.preventDefault();
+    var hashedPass = hashSync(password, 10)
+    console.log(hashedPass)
+    fetch('http://localhost:5000/register', {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json',
+      },
+      body: JSON.stringify({
+        'email': email,
+        'password': hashedPass
+      })
+    })
+      .then((response) => {
+        console.log(response.body)
+        // document.getElementById('regForm').reset()
+      })
+  }
   return (
     <>
       <Link to="/">
@@ -19,11 +43,27 @@ const Signup = () => {
         <div className="login">
           <div className="header">Create Your Account</div>
           <div className="fields">
-            <input type="text" placeholder="Email" />
-            <input type="password" placeholder="Password" />
-            <input type="password" placeholder="Confirm Password" />
+          <input type="text" id='email'
+              name='email'
+              value={email}
+              required={true}
+              onChange={(e) => setEmail(e.target.value)}
+              placeholder="Email" />
+            <input type="password"
+              id='password'
+              name='password'
+              value={password}
+              required={true}
+              onChange={(e) => setPassword(e.target.value)}
+              placeholder="Password" />
+            <input type="password" id='confpassword'
+              name='confpassword'
+              value={confpassword}
+              required={true}
+              onChange={(e) => setConfPassword(e.target.value)}
+              placeholder="Confirm Password" />
             <Link to="/">
-              <button type="button" className="login-btn">
+              <button type="button" className="login-btn" onClick={HandleSignup}>
                 Sign up
               </button>
             </Link>
